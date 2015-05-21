a74e83c1a48ae1782ee3bad0ab5c07a459288432
abhorrentPantheon <abhorrentPantheon@users.noreply.github.com>
2015-05-21 15:49:49 +1000

    Added WebGL build, cursor, tags.
    Arrows and boxes tagged so that initLoc works nicely (prev commit as
    well). Build created for WebGL. Cursor made.

--- Assets/scripts/MouseManager.cs
+++ Assets/scripts/MouseManager.cs
@@ -6,6 +6,7 @@ public class MouseManager: MonoBehaviour {
 	
 	Rigidbody2D grabbedRB = null;
 	Vector2 grabbedInitPos = Vector2.zero;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -24,14 +25,15 @@ public class MouseManager: MonoBehaviour {
 			
 			//if (hit != null && hit.collider != null) {
 			if (hit.collider != null) {
-				// Clicked on something with a collider
-				//Debug.Log ("clicked on " + hit.collider.name);
+
 				grabbedRB = hit.collider.attachedRigidbody;
-				//grabbedInitPos = GameObject.Find(grabbedRB.name).GetComponent<InitVelocity>().initLoc;
 				grabbedInitPos = GameObject.Find(grabbedRB.name).GetComponent<objMovement>().initLoc;
+
+				// Set reHome to false so that it doesn't conflict with other movement in update
+				//GameObject.Find(grabbedRB.name).GetComponent<objMovement>().reHome = false;
+
 			}
 
-			
 		}
 		
 		if (Input.GetMouseButtonUp(0)) {
@@ -43,33 +45,6 @@ public class MouseManager: MonoBehaviour {
 				grabbedRB.velocity = Vector2.zero;
 				//Debug.Log ("let go of " + grabbedRB.name);
 
-				// If ovObj is a box, move it to a socket
-				if (grabbedRB.name.Contains( "box") ) {
-					// Create alias for detect overlap component
-					var detOver = GameObject.Find(grabbedRB.name).GetComponent<DetectOverlap>();
-					
-					if (detOver.overlapObj != null) {
-						List<string> ovList = detOver.overlapList;
-						//string ovListP = string.Join("-", ovList.ToArray());
-						
-						// List comprehension to find 'target' in strings of ovList
-						string ovObj = ovList.Find(listEl => listEl.Contains("socket"));
-						
-						// If there's a 'target' for the object:
-						if (ovObj != null ) {
-							grabbedRB.position = GameObject.Find(ovObj).transform.position;
-							
-						} else {
-							// otherwise snap back to initLoc
-							grabbedRB.position = grabbedInitPos;
-						}
-						
-						// If there's no overlap of a 'target', snap back to initLoc
-					} else {
-						grabbedRB.position = grabbedInitPos;
-					}
-				}
-
 				/* 
 				 * 
 				 * This sections need to detect whether socket/target is already populated, and if so, return object
@@ -77,49 +52,43 @@ public class MouseManager: MonoBehaviour {
 				 * 
 				 */
 
+				/*
+				 * // If ovObj is a box, move it to a socket
+				 * 
+				 */
+
 				// If ovObj is an arrow, move it to a target
 				if (grabbedRB.name.Contains( "arrow") ) {
 					// Create alias for detect overlap component
 					var detOver = GameObject.Find(grabbedRB.name).GetComponent<DetectOverlap>();
+					var objMov = GameObject.Find(grabbedRB.name).GetComponent<objMovement>();
 
 					if (detOver.overlapObj != null) {
 						List<string> ovList = detOver.overlapList;
 						//string ovListP = string.Join("-", ovList.ToArray());
 
 						// List comprehension to find 'target' in strings of ovList
-						string ovObj = ovList.Find(listEl => listEl.Contains("target"));
+						string ovObj = ovList.Find(listOvObj => listOvObj.Contains("target"));
 
 						// If there's a 'target' for the object:
 						if (ovObj != null ) {
 							grabbedRB.position = GameObject.Find(ovObj).transform.position;
+							objMov.atDest = true;
+						} else {
+							objMov.atDest = false;
+							//Vector2 goHome = grabbedInitPos - grabbedRB.position;
+							Debug.Log (grabbedInitPos - grabbedRB.position);
+							//grabbedRB.velocity = grabbedInitPos - grabbedRB.position;
+							//grabbedRB.velocity = Vector2.Lerp(grabbedRB.position, grabbedInitPos, Time.deltaTime);
 
-						} 
-						//else {
-							// otherwise snap back to initLoc
-							//grabbedRB.position = grabbedInitPos;
+						}
+					} else {
+						objMov.atDest = false;
+						//grabbedRB.velocity = grabbedInitPos - grabbedRB.position;
+						Debug.Log (grabbedInitPos - grabbedRB.position);
 
-							//Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-							//Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
-							//Vector2 returnHome = grabbedRB.position - grabbedInitPos;
-//							Vector2 returnHome = grabbedInitPos - grabbedRB.position;
-//							Debug.Log (returnHome);
-							//Vector2 returnHome = grabbedInitPos - mousePos2D;
-//							grabbedRB.velocity = returnHome;// * 25;
-//						}
-
-					// If there's no overlap of a 'target', snap back to initLoc
-					} 
-					//else {
-						//grabbedRB.position = grabbedInitPos;
-
-						//Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-						//Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
-						//Vector2 returnHome = grabbedRB.position - grabbedInitPos;
-//						Vector2 returnHome = grabbedInitPos - grabbedRB.position;
-//						Debug.Log (returnHome);
-						//Vector2 returnHome = grabbedInitPos - mousePos2D;
-//						grabbedRB.velocity = returnHome;// * 25;
-//					}
+						//grabbedRB.velocity = Vector2.Lerp(grabbedRB.position, grabbedInitPos, Time.deltaTime);
+					}
 				}
 				// null MUST come after doing something with it, else exception.
 				grabbedRB = null;
@@ -131,30 +100,36 @@ public class MouseManager: MonoBehaviour {
 	
 	void FixedUpdate() {
 		// Don't move if it's a blank box
-		if (grabbedRB != null && !grabbedRB.name.Contains("blank")) {
-			
+		//if (grabbedRB != null && !grabbedRB.name.Contains("blank")) {
+		if (grabbedRB != null) {
 			// Make the object centre to the mouse position smoothly
 			Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
 			
 			Vector2 dir = mousePos2D - grabbedRB.position;
-
 			// move smoothly to mouse position
 			grabbedRB.velocity = dir * 25;
-
 			// Snap to mousePos2D:
 			//grabbedObject.position = mousePos2D;
 
 			/* 
 			 * Fix this bit to return objects to home smoothly (rather than instant 
 			 */
+		}
 
-//			var objMov = GameObject.Find(grabbedRB.name).GetComponent<objMovement>();
-//			if (!objMov.atHome) {
-//				//Vector2 retHome = grabbedRB.position - grabbedInitPos;
-//				Vector2 retHome = grabbedInitPos - grabbedRB.position;
-//				grabbedRB.velocity = retHome * 25;
-//			}
+		foreach(GameObject arw in GameObject.FindGameObjectsWithTag("arrows")) {
+			var aom = arw.GetComponent<objMovement>();
+			if (aom.reHome) {
+				float spd = 100 * Time.deltaTime;
+				arw.transform.position = Vector2.MoveTowards(arw.transform.position, aom.initLoc, spd);
+			}
+		}
+		foreach(GameObject bxs in GameObject.FindGameObjectsWithTag("boxes")) {
+			var bom = bxs.GetComponent<objMovement>();
+			if (bom.reHome) {
+				float spd = 100 * Time.deltaTime;
+				bxs.transform.position = Vector2.MoveTowards(bxs.transform.position, bom.initLoc, spd);
+			}
 		}
 	}
 }

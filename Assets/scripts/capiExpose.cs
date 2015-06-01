e29c60f33f36597ef45f3ed58a7e76483c988487
abhorrentPantheon <abhorrentPantheon@users.noreply.github.com>
2015-06-01 17:16:49 +1000

    CAPI vars exposed, working in SS
    While the variables are present, need to also expose (ans|arr)Missing
    for feedback. Need to get feedback into answer produced by Smart Sparrow
    too.

---
+++ Assets/scripts/capiExpose.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class capiExpose : MonoBehaviour {
+	
+	/* Create CAPI variables */
+	//private static int tmpvar = GameObject.Find("button_done").GetComponent<pathwayAAScore>().outScore;
+	//private int _outScore = tmpvar;
+	private int _outScore = 0;
+	public int oScore {
+		get { return _outScore; }
+		set { _outScore = value; Capi.set ( "Sim.Score", value ); }
+	}
+	//private string _outFeed = GameObject.Find("button_done").GetComponent<pathwayAAScore>().outFeed;
+	private string _outFeed = "";
+	public string oFeed {
+		get { return _outFeed; }
+		set { _outFeed = value; Capi.set ( "Sim.Feedback", value ); }
+	}
+	
+	// Use this for initialization
+	void Start () {
+
+		/* Change output values */
+		oScore = this.GetComponent<pathwayAAScore>().outScore;
+		oFeed = this.GetComponent<pathwayAAScore>().outFeed;
+
+		/* Allow CAPI variables to be seen */ 
+		Capi.expose<int> ("Sim.Score", () => { return oScore; }, (value) => { return oScore = value; } );
+		Capi.expose<string> ("Sim.Feedback", () => { return oFeed; }, (value) => { return oFeed = value; } );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
